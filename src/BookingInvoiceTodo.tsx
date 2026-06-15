0f330d0ae6f32652861947fe3ba5121d957c2d49
Nathan <nathan@theloft>
2026-06-15 09:39:09 +0000

    Sort booking by firstSeen desc (newest added first)

--- src/BookingInvoiceTodo.tsx
+++ src/BookingInvoiceTodo.tsx
@@ -277,7 +277,7 @@ export default function BookingInvoiceTodo() {
   const invoicePending  = data.invoice.filter(x => !x.done).length;
   const invoiceNewToday = data.invoice.filter(x => x.detectedToday && !x.done).length;
 
-  const visibleBooking = data.booking.filter(x => showDoneBooking || !x.done).sort((a, b) => b.checkin > a.checkin ? 1 : -1);
+  const visibleBooking = data.booking.filter(x => showDoneBooking || !x.done).sort((a, b) => { const fa = a.firstSeen || a.checkin; const fb = b.firstSeen || b.checkin; return fb > fa ? 1 : -1; });
   const visibleInvoice = data.invoice.filter(x => showDoneInvoice || !x.done).sort((a, b) => b.detectedDate > a.detectedDate ? 1 : -1);
 
   return (
